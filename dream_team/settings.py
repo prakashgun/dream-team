9c97810c4fa780ad5d803960aa95d293c3ba353d
prakashgun <mail.prakash.india@gmail.com>
2020-10-18 06:40:41 +0530

    Created user management app

--- dream_team/settings.py
+++ dream_team/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     # App
+    'user_management',
     'performance'
 ]
 
--- dream_team/urls.py
+++ dream_team/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from performance.views import UserViewSet, GroupViewSet
-from rest_framework import routers
-
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user-management/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('user-management/', include('user_management.urls'))
 ]
